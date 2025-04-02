89d5805b1513aaee64e8916553f7b34fd7e65199
ozeemm <77226278+ozeemm@users.noreply.github.com>
2025-04-03 01:20:04 +0800

    Lab 1.3 done

---
+++ 1. Multithreading/Lab3. Synchronization/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Lab3._Synchronization
+{
+    internal class Program
+    {
+        private static double value = 0.5;
+        private static Mutex mutex = new();
+
+        private static int itersCount = 10;
+        static void Main(string[] args)
+        {
+            var cosThread = new Thread(() => Cos(itersCount)) { Name="Cos-Thread" };
+            var arccosThread = new Thread(() => Arccos(itersCount)) { Name="Arccos-Thread" };
+
+            cosThread.Start();
+            arccosThread.Start();
+
+            cosThread.Join();
+            arccosThread.Join();
+
+            Console.WriteLine("Done!");
+        }
+
+        static void Cos(int itersCount)
+        {
+            for (int i = 0; i < itersCount; i++)
+            { 
+                mutex.WaitOne();
+                value = Math.Cos(value);
+                Console.WriteLine($"[{Thread.CurrentThread.Name}]: {value}");
+                mutex.ReleaseMutex();
+            }
+        }
+
+        static void Arccos(int itersCount)
+        {
+            for (int i = 0; i < itersCount; i++)
+            {
+                mutex.WaitOne();
+                value = Math.Acos(value);
+                Console.WriteLine($"[{Thread.CurrentThread.Name}]: {value}");
+                mutex.ReleaseMutex();
+            }
+        }
+    }
+}
